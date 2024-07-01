0d801ff359322eb17634b1c7b2a9f07473960af8
Arati <arti.mahajan86@gmail.com>
2024-07-01 15:07:27 -0700

    be-db-sertup

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+
 const {
   NODE_ENV = "development",
   DEVELOPMENT_DATABASE_URL,
   PRODUCTION_DATABASE_URL,
 } = process.env;
+
 const URL =
   NODE_ENV === "production"
     ? PRODUCTION_DATABASE_URL
